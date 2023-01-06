04bcbfb3ae86462763958f26c3c39b6a88b7191b
VidBrloznik <121978367+VidBrloznik@users.noreply.github.com>
2023-01-06 12:07:23 +0100

    nadaljevanje druge naloge

--- Task2/main.c
+++ Task2/main.c
@@ -2,33 +2,44 @@
 #include <string.h> 
 #include <stdlib.h>
 int main() {
-//osnovne deklaracije
+    int prav=0;
     int zivljenj=10;
     char crka;
-    char besede[5][5]={"besed","kebab","smrkc"};
+    char besede[5][5]={"besed","kebab","smrkc"}; //možnosti besed
     char ugani[]={'_','_','_','_','_'};
     int a;
     printf("Vpisi stevilo med 1 in 3: ");
     scanf("%i",&a);  //izbira besede
-    for(int i=0;i<5;i++){ //loop za uganjanje črk
+    do{ //loop za uganjanje črk
         int p=0;
-        printf("vpisi crko: ");
+        printf("vpisi crko: "); //vpis črke 
         scanf("%s" , &crka);
         printf("\n");
-        for(int j=0;j<5;j++){   //loop ki gleda pravilen vpis in zamenja _ z črkoe 
-            if(crka==besede[a-1][j]){
+        for(int j=0;j<5;j++){
+            if(crka==besede[a-1][j]){ //primerjava vpisane črke z besedo
                 ugani[j]=crka;
+                p++;   //pregled pravilnih znakov in števec za odpis življenja
+                prav++;
             }
         }
-        for(int i=0;i<5;i++){  //izpiše _ , _ ,... z uganjenimi črkami
+        for(int i=0;i<5;i++){   //izpiše _ , _ ,... z uganjenimi črkami
             printf("%c" , ugani[i]);
          if(i!=4){
          printf(" , ");
          }
         }
         printf("\n");
+            zivljenj--;
+        printf("imate se %i pojskusov \n",zivljenj);
         
     }
+    while(zivljenj>0 || prav==5); //pogoji za konec ugotavljanja
     
+    if(zivljenj==0){   //izpis zmage/poraza
+        printf("PORAZ");
+    }
+    else{
+        printf("ZMAGA");
+    }
     return 0;
 }
